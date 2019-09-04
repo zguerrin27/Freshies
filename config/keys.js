bec70786828d316ad6dea1b6c97076ec27b32968
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-09-04 10:13:13 -0400

    Completed Most of Backend... GET/POST/DELETE routes working with DB on Mongodb Atlas

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb+srv://zguerrin27:105ec333@freshies-cluster0-980ei.mongodb.net/test?retryWrites=true&w=majority"
+};
---
+++ routes/items.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+// ^^^ pull in express and express.router from the express model
+
+// Bring in item Model to make queries 
+const Item = require('../models/Item');
+
+// route is GET /items, It gets all items, if I was adding auth it would be Public
+router.get('/', (req, res) => {
+  Item.find()
+  .then(items => res.json(items))
+})
+
+// route is POST /items, Creates an item, if I was adding auth it would be Private
+router.post('/', (req, res) => {
+  // construct a new item with Item Model
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  // save the new item to DB, the promise returns the item and we want it to come back as JSON
+  newItem.save().then(item => res.json(item))
+})
+
+// route is DELETE /items/:id, It gets all items, if I was adding auth it would be Public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+ // with promise back to us..take item arrow func.remove item..promise is then callback..make the res an object. 
+  .then(item => item.remove().then( () => res.json({success: true})))
+  .catch(err => res.status(404).json({success: false}))
+})
+
+
+
+
+module.exports = router;
