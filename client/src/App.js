aca59a92be630adac52e214368b5ef32eadad525
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-09-04 11:10:29 -0400

    Setup client with react app..got navbar  setup

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppNavBar from './components/AppNavbar'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import AppNavbar from './components/AppNavbar';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar></AppNavbar>
+    </div>
+  );
+}
+
+export default App;
